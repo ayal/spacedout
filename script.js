cfa43598ebc33e755fe427544f53d2082acb574d
ayalgelles <ayal.gelles@simplfied-networks.com>
2015-04-02 22:29:35 +0300

    spacedout

--- script.js
+++ script.js
@@ -1,37 +1,72 @@
-// Code goes here
-$(function(){
+var Spacedout = React.createClass({
+    getInitialState:function() {
+        return {val:this.props.val, style:this.props.style, cols:this.props.cols};
+    },
+    valChanged: function() {
+        var val = event.target.value;
+        this.setState({val: val});
+    },
+    changeHeight: function() {
+        var val = event.target.value;
+        var style = _.clone(this.state.style);
+        style.height = val;
+        this.setState({style: style});
+    },
+    changeWidth: function() {
+        var val = event.target.value;
+        var style = _.clone(this.state.style);
+        style.width = val;
+        this.setState({style: style});
+    },
+    changeCols: function() {
+        var val = event.target.value;
+        this.setState({cols: val});
+    },
+    render: function() {
+        var spacedout = this;
+        var val = this.state.val;
 
-var cols = 6;
-sowidth = $('.spacedout').width();
-soheight = $('.spacedout').height();
+        grid = _.groupBy(val.split(''), function(l, i) {
+            return Math.floor(i / spacedout.state.cols);
+        });
 
-var val;
 
-$('textarea').keypress(function() {
-  val = $(this).val()
-  doSpacedOut(val);
-})
+        grid =
+                _.map(
+                    _.map(grid, function(x) {
+                        return _.map(x, function(y) {
+                            var h = spacedout.state.style.height / Math.ceil(val.length / spacedout.state.cols);
+                            return <span className="col" style={{'font-size': h * 0.5, width: spacedout.state.style.width / spacedout.state.cols, height: h}}>{y}</span>;
+                        });
 
-doSpacedOut = function(val) {
-  grid = _.groupBy(val.split(''), function(l, i) {
-    return Math.floor(i / cols)
-  });
+                    }), function(z) {
+                        return <div className="row">{z}</div>;
+                    });
 
-  var cc = 
-  _.map(
-  _.map(grid, function(x) {
-    return _.map(x, function(y) {
-      var h = soheight / (val.length / cols);
-      return $('<span class="col" />').css({'font-size': h * 0.5, width: sowidth / cols, height: h }).text(y)[0].outerHTML;
 
-    })
+        return (
+<div>
+                <div className="editor">
+                <textarea value={this.state.val} onChange={this.valChanged}></textarea>
+                <input value={this.state.style.height} onChange={this.changeHeight} />
+                <input value={this.state.style.width} onChange={this.changeWidth} />
+                <input value={this.state.cols} onChange={this.changeCols} />
+                </div>
+<div>
+                <pre className="spacedout" style={this.state.style}>
+                {grid}
+            </pre>
 
-  }), function(z){
-    return $('<div class="row" />').html(z.join(''))[0].outerHTML;
-  })
-  
-  
-  $('.spacedout').html(cc.join(''));
-}
-  
-})
+                </div>
+
+                </div>
+        );
+    }
+});
+
+
+var spacedoutstyle = {width:200,height:200,border:'6px solid white', padding:30, 'font-family': "futura-pt",'font-weight':500,color:'white'};
+React.render(
+  <Spacedout cols={3} val="spacedout" style={spacedoutstyle} />,
+  document.getElementById('container')
+);
